6d4ad6de3eef79e4de97fa856f72bf5de5679947
Patrik Chytil <patrik.chytil@inventi.cz>
2016-12-16 15:31:42 +0100

    ubungo module creation

---
+++ ubungo.py
@@ -0,0 +1,62 @@
+import math
+
+class Card:
+    
+    def __init__(self):
+        self.shape = []
+
+    def try_put_together(self, pieces):
+        pass
+
+    def assign_piece(self, shape, piece):
+        """
+        assign somehow and return empty rest shape
+        """
+
+class Piece:
+    """ """
+    
+    def draw(self):
+        pass
+
+class Cell:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, cell):
+        return self.x == cell.x and self.y == cell.y
+
+    def has_touch(self, cell):
+        return (self.x == cell.x and int(math.fabs(self.y - cell.y))==1) or \
+               (self.y == cell.y and  int(math.fabs(self.x - cell.x))==1)
+    
+
+class Block:
+    
+    def __init__(self, start=(0,0), end=(0,0)):
+        self.start_x = start[0]
+        self.start_y = start[1]
+        self.end_x = end[0]
+        self.end_y = end[1]
+        self.cells = []
+
+    def draw(self):
+        """"""
+
+    def __add__(self, block):
+        """
+        if blocks doent touch each other throw ValueError
+        otherwise return their sum block
+        """
+        # 1.prunik
+
+    def _intersection(self, cells, other_cells):
+        results = [x == y for x in cells for y in other_cells]
+        return any(results)
+
+    def _touch(self, cells, other_cells):
+        results = [x.has_touch(y) for x in cells for y in other_cells]
+        return any(results)
+                
+    
